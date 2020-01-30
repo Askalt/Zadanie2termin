b4e1864913df9c71ae056c217bcd632e79e0f945
Jakub Sałamun <zellanderplx@gmail.com>
2020-01-30 17:39:25 +0100

    as

---
+++ Zalicz_2_termin/Zalicz_2_termin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zalicz_2_termin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
